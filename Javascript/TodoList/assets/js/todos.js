bd68febabd7c8bb228962a1d64339940ba133672
David Oliveira <dol.jpo@hotmail.com>
2019-10-31 11:39:06 -0300

    Added delete function

--- Javascript/TodoList/assets/js/todos.js
+++ Javascript/TodoList/assets/js/todos.js
@@ -1,3 +1,9 @@
-$("li").on("click", function() {
+$("li").on("click", function() {    
     $(this).toggleClass('complete');
+});
+
+$("li span").on("click", function() {    
+    $(this).parent().fadeOut(1000, function() {
+        $(this).remove();
+    });
 });
