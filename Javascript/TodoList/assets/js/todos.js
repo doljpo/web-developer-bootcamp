6f065df49ab885ae0fcd708e7748bd9a9fc04041
David Oliveira <dol.jpo@hotmail.com>
2019-10-31 14:25:32 -0300

    Added function to insert new todos

--- Javascript/TodoList/assets/js/todos.js
+++ Javascript/TodoList/assets/js/todos.js
@@ -1,9 +1,17 @@
-$("li").on("click", function() {    
+$("ul").on("click", "li", function() {    
     $(this).toggleClass('complete');
 });
 
-$("li span").on("click", function() {    
+$("ul").on("click", "li span", function() {    
     $(this).parent().fadeOut(1000, function() {
         $(this).remove();
     });
+});
+
+$("input[type=text]").on('keypress', function(key) {
+        if (key.which == 13) {
+            var newTodo = $(this).val();
+            $(this).val("");
+            $("ul").append("<li><span>X</span> " + newTodo + "</li>");
+        }
 });
